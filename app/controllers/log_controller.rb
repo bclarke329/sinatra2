55d6d14abe74917b11a5c3b70a4fd8c4a2ab7ba0
bclarke329 <bmendoza329@hotmail.com>
2020-11-21 23:41:31 -0600

    Delete route works/edit still needs work

--- app/controllers/log_controller.rb
+++ app/controllers/log_controller.rb
@@ -6,42 +6,50 @@ class LogController < ApplicationController
             erb :'/logs/index' 
     end
 
+    get '/logs/new' do #new, shows form 
+        erb :'logs/new'
+    end 
+    
+
     get '/logs/:id' do #show
         @log = Log.find_by_id(params[:id])
         if @log
             erb :'/logs/show'
         else
-            "That log does not exist"
+            erb :'/failure'
         end 
     end 
  
-    get '/logs/new' do #new, shows form 
-             erb :'/logs/new'
-        end 
-    
-    post '/logs' do #create the new instance 
-        @log = Log.create(:current_condition => params[:current_condition], :new_products => params[:new_products], :list_products => params[:list_products], :comments => params[:comments])
-        @log.user_id = current_user.id
+  
+    post '/logs' do #create 
+        @log = Log.create(params)
+        # @log.user_id = current_user.id
         if @log.save 
-        redirect to "/logs/#{@log.user_id}"   
+        redirect to "/logs/#{@log.id}"   
         else
             erb :"logs/new"   
-            end
-        end 
+        end
+    end 
 
-    # get '/logs/:id/edit' do 
-    #     displays edit form based on ID in url
-    # end 
+    get '/logs/:id/edit' do 
+        @log = Log.find_by_id(params[:id])
+          erb :'/logs/edit' 
+    end 
 
-    # patch '/logs/:id' do 
-    #     modifies an existing log based on id in the URL
-    # end
+    patch '/logs/:id' do 
+        log = Log.find_by_id(params[:id])
+        log.update(params[:current_condition])
+        log.update(params[:new_product])
+        log.update(params[:list_product])
+        log.update(params[:comment])
+        log.save
+        # binding.pry
+        redirect to "/logs/#{log.id}"  
+    end
 
-    # put '/logs/:id' do 
-    #     replaces an existing log
-    # end 
-
-    # delete '/logs/:id' do 
-    #     deletes a log baed on the id in the url
-    # end 
+    delete '/logs/:id' do #destory
+        log = Log.find_by_id(params[:id])
+        log.destroy
+        redirect to '/logs'
+    end 
 end 
